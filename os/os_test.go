a70894da97cfb82b238cd5a3d49929e10e2db1ad
wuliuqi <1737682009@qq.com>
2024-07-29 14:07:17 +0800

    feat:检测网络状态

--- os/os_test.go
+++ os/os_test.go
@@ -1,6 +1,10 @@
 package os
 
-import "testing"
+import (
+	"os/exec"
+	"testing"
+	"time"
+)
 
 func TestGetCurrentProcessID(t *testing.T) {
 	t.Log(GetCurrentProcessID())
@@ -12,4 +16,17 @@ func TestGetCurrentGoroutineID(t *testing.T) {
 
 func TestGetCurrentProcessAndGoroutineIDStr(t *testing.T) {
 	t.Log(GetCurrentProcessAndGoroutineIDStr())
+}
+
+func TestNetworkStatus(t *testing.T) {
+	cmd := exec.Command("ping", "www.baidu.com", "-n", "1", "-w", "3")
+	t.Log("network status start", time.Now().Unix())
+	t.Log(cmd.String())
+	err := cmd.Run()
+	t.Log("network status end :", time.Now().Unix())
+	if err != nil {
+		t.Log("network status false :", err.Error())
+	} else {
+		t.Log("network status true")
+	}
 }
